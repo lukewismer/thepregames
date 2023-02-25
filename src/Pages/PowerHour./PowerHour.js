2ed5ddb91a13a84584c370ed42f509baa4251369
lukewismer <lukewismer77@gmail.com>
2023-02-24 17:03:53 -0700

    Gif functionality

--- src/Pages/PowerHour./PowerHour.js
+++ src/Pages/PowerHour./PowerHour.js
@@ -12,18 +12,24 @@ const PowerHour = () => {
   const location = useLocation();
   const songs = location.state.playlistSongs;
   const songInterval = location.state.songInterval;
-  const tornadoInterval= location.state.tornadoInterval;
+  const tornadoInterval= 60000;
   const players = location.state.namesList;
 
+  const images = ["https://giphy.com/embed/ebP9BKoQbsIRMKNNw8", "https://giphy.com/embed/7FgDOBGyFLgns6aiKb", "https://giphy.com/embed/1mNBTj3g4jRCg","https://giphy.com/embed/TLulTJKuyLgMU", "https://giphy.com/embed/FQ6pL4icGpKH6",
+                "https://giphy.com/embed/xAFcpblRNMyfvt1oAf", "https://giphy.com/embed/c5paNX4a8hc6A", "https://giphy.com/embed/H9Mm0ULO8AtMc", "https://giphy.com/embed/10H4by255F2UsU", "https://giphy.com/embed/l0HU4QhliQGCcelYk",
+                "https://giphy.com/embed/pLcVMwRRltmUg", "https://giphy.com/embed/3oz8xxpsXv8pzPgXPG", "https://giphy.com/embed/Y0OXD3S690kwGPDiga", "https://giphy.com/embed/rfZCdXOgS5aDK", "https://giphy.com/embed/BRElOZZ2eOn3q",
+                "https://giphy.com/embed/TGWkzI2QdUmvdSz1oa", "https://giphy.com/embed/BCIoXfA95d1ba", "https://giphy.com/embed/wSlEV7ztEQ0zZ3b9Cm", "https://giphy.com/embed/3kzrzzQXUfI6bmUNf3", "https://giphy.com/embed/xUPGcB5L0TIfLrDH8c", "https://giphy.com/embed/IbchZ7B1ulecFMRNv0"]
+  
+  
   const [currentSongIndex, setCurrentSongIndex] = useState(selectRandom(songs));
   const [time, setTime] = useState({ min: 0, sec: 0});
   const [isRunning, setIsRunning] = useState(false);
-  const [tornadoImage, setTornadoImage] = useState(null);
-  const [tornadoPerson, setTornadoPerson] = useState(null);
+  const [tornadoImage, setTornadoImage] = useState(images[selectRandom(images)]);
+  const [tornadoPerson, setTornadoPerson] = useState(players[selectRandom(players)]);
   const [tornadoTime, setTornadoTime] = useState(false);
 
-  const tornadoDisplayTime = 5000;
-  const images = [require("../../Images/cat.jpeg")]
+  const tornadoDisplayTime = 10000;
+  
   const navigate = useNavigate();
 
 
@@ -47,19 +53,23 @@ const PowerHour = () => {
             newMin++;
           }
 
+
           return { min: newMin, sec: newSec};
         });
 
       }, 1000);
 
       // Show image every 3 minutes
-      timeoutId = setTimeout(() => {
-        setTornadoImage(preImg => images[selectRandom(images, images.indexOf(preImg))]);
-        setTornadoPerson(prevPerson => players[selectRandom(players, players.indexOf(prevPerson))]);
+      timeoutId = setInterval(() => {
         setTornadoTime(true);
-        setTimeout(() => {
-          setTornadoTime(false);
-        }, tornadoDisplayTime);
+            setTimeout(() => {
+              setTornadoTime(false);
+              const newImg = images[selectRandom(images, images.indexOf(tornadoImage))];
+              const newPerson = players[selectRandom(players, images.indexOf(tornadoPerson))];
+              setTornadoImage(newImg);
+              setTornadoPerson(newPerson);
+
+            }, tornadoDisplayTime);
       }, tornadoInterval);
   
       
@@ -68,7 +78,7 @@ const PowerHour = () => {
     return () => {
       clearInterval(intervalId);
       clearInterval(intervalId2);
-      clearInterval(timeoutId);
+      //clearInterval(timeoutId);
     };
   }, [isRunning, songs.length]);
   
@@ -96,7 +106,7 @@ const PowerHour = () => {
         <div>
           {tornadoTime ? (
             <>
-              <img src={tornadoImage} alt="tornado Time"></img>
+            <iframe src={tornadoImage} width="478" height="480" class="giphy-embed" allowFullScreen></iframe>
               <h2>Time to Tornado: {tornadoPerson}</h2>
             </>
           ) : (
